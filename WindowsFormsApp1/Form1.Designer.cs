50b4a8735f1e647f7ae510ca836e36296910161c
Mehmet Akın <kaygerya@hotmail.com>
2018-11-13 22:25:54 +0300

    .

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContatcsGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ContatcsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ContatcsGridView
+            // 
+            this.ContatcsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ContatcsGridView.Location = new System.Drawing.Point(13, 26);
+            this.ContatcsGridView.Name = "ContatcsGridView";
+            this.ContatcsGridView.Size = new System.Drawing.Size(500, 150);
+            this.ContatcsGridView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(54, 221);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(257, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(54, 251);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(257, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(54, 281);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(257, 20);
+            this.txtPhone.TabIndex = 7;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(334, 279);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(103, 23);
+            this.BtnAdd.TabIndex = 8;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(16, 326);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 348);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ContatcsGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ContatcsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ContatcsGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Contacts;
+using Google.GData.Extensions;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.GData.Client;
+using Google.GData.Contacts;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Auth.OAuth2.Flows;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Form1));
+
+        //client secret and client public key
+        string clientId = "899990018154-ni7b6dkoog9qbleu3jm57k58r5ogkqu2.apps.googleusercontent.com";
+        string clientSecret = "ceraVKARO_i7TYPH30qCXODE";
+        string refreshToken = "";
+        TokenResponse tokenResponse = new TokenResponse();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //authenticate at startup
+            Authenticate();
+
+        }
+
+        public void Authenticate()
+        {
+
+            //scope
+            string[] scopes = new string[] { "https://www.google.com/m8/feeds/" };     // view your basic profile info.
+            try
+            {
+                ClientSecrets secrets = new ClientSecrets();
+                secrets.ClientId = clientId;
+                secrets.ClientSecret = clientSecret;
+                // Use the current Google .net client library to get the Oauth2 stuff.
+                UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(secrets
+                                                                                             , scopes
+                                                                                             , "test"
+                                                                                             , CancellationToken.None
+                                                                                             , new FileDataStore("test")).Result;
+
+
+                // Translate the Oauth permissions to something the old client libray can read
+                tokenResponse = credential.Token;
+
+                //older date shoud be renew the token.  token has 1 hour only
+                if (credential.Token.IssuedUtc < DateTime.UtcNow)
+                {
+                    //refresh token process
+                    RefreshToken(secrets, scopes);
+
+                }
+
+                //fetch contacts from google
+                RunContactsSample();
+            }
+            catch (Exception ex)
+            {
+                //log Error
+                log.Error("Authentication Error");
+            }
+
+
+
+        }
+
+        private void RunContactsSample()
+        {
+
+            try
+            {
+                OAuth2Parameters parameters = new OAuth2Parameters();
+                parameters.AccessToken = tokenResponse.AccessToken;
+                parameters.RefreshToken = tokenResponse.RefreshToken;
+
+                RequestSettings settings = new RequestSettings("Google contacts tutorial", parameters);
+                // max contact count
+                settings.PageSize = int.MaxValue;
+                ContactsRequest cr = new ContactsRequest(settings);
+                Feed<Contact> f = cr.GetContacts();
+
+                DataTable table = new DataTable();
+                table.Columns.Add("Name".ToString());
+                table.Columns.Add("Email".ToString());
+                table.Columns.Add("Phone".ToString());
+
+                DataRow headerRow = table.NewRow();
+                headerRow["Name"] = "FullName";
+                headerRow["Email"] = "Email";
+                headerRow["Phone"] = "Phone";
+
+                table.Rows.Add(headerRow);
+
+                if (f.Entries.Count() == 0)
+                {  //log row count Info
+                    log.Info("Contacts fetched" + " " + "0 row");
+                }
+                foreach (Contact c in f.Entries)
+                {
+                    DataRow dr = table.NewRow();
+                    dr["Name"] = c.Name.FullName;
+                    dr["Email"] = c.Emails.FirstOrDefault()?.Address;
+                    dr["Phone"] = c.Phonenumbers.FirstOrDefault()?.Uri;
+                    table.Rows.Add(dr);
+                }
+
+                ContatcsGridView.DataSource = table;
+                ContatcsGridView.Columns[0].Width = 100;
+                ContatcsGridView.Columns[1].Width = 150;
+                ContatcsGridView.Columns[2].Width = 150;
+
+                //log row count Info
+                log.Info("Contacts fetched" + " " + table.Rows.Count);
+            }
+            catch (Exception a)
+            {
+                //log Error
+                log.Error("Fetch Contatcs Error");
+            }
+        }
+
+        public void RefreshToken(ClientSecrets secrets, string[] scopes)
+        {
+            IAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = secrets,
+                Scopes = scopes
+            });
+
+            UserCredential credential = new UserCredential(flow, "me", tokenResponse);
+            bool success = credential.RefreshTokenAsync(CancellationToken.None).Result;
+
+            tokenResponse = credential.Token;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //older date shoud be renew the token.  token has 1 hour only
+                if (tokenResponse.IssuedUtc < DateTime.UtcNow)
+                {
+                    //reauthenticate
+                    Authenticate();
+                }
+                OAuth2Parameters parameters = new OAuth2Parameters();
+                parameters.AccessToken = tokenResponse.AccessToken;
+                parameters.RefreshToken = tokenResponse.RefreshToken;
+
+                RequestSettings settings = new RequestSettings("Google contacts tutorial", parameters);
+                // max contact count
+                ContactsRequest cr = new ContactsRequest(settings);
+
+                // new entry
+                Contact entry = new Contact();
+                entry.Name.FullName = txtName.Text;
+                EMail email = new EMail(txtEmail.Text);
+                PhoneNumber phonenumber = new PhoneNumber(txtPhone.Text);
+                phonenumber.Rel = "http://schemas.google.com/g/2005#work";
+                entry.Phonenumbers.Add(phonenumber);
+
+                Uri feedUri = new Uri(ContactsQuery.CreateContactsUri("default"));
+                Contact createdEntry = cr.Insert<Contact>(feedUri, entry);
+
+                lblResult.Text = "Successfully Added: " + createdEntry.Name.FullName;
+                lblResult.Visible = true;
+                lblResult.ForeColor = Color.Green;
+
+                RunContactsSample();
+            }
+            catch(Exception ex)
+            {
+                lblResult.Text = ex.Message;
+                lblResult.Visible = true;
+                lblResult.ForeColor = Color.Red;
+                log.Error(ex.Message);
+            }
+        }
+
+    }
+}
+
